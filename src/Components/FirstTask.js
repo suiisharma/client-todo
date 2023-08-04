443b33d22cc32253b7dd75a5106681d685aa7dcd
sourav <95113886+bawanegi@users.noreply.github.com>
2023-08-04 20:03:18 +0530

    3rd time

--- src/Components/FirstTask.js
+++ src/Components/FirstTask.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box, Text, useColorMode } from "@chakra-ui/react";
 import { Tilt } from "react-tilt";
 
@@ -6,33 +6,23 @@ const ThreeDCard = () => {
   const { colorMode } = useColorMode();
   const borderColor = colorMode === "light" ? "gray.600" : "gray.200";
 
-  const isMobile = window.innerWidth <= 768; // Define your breakpoint for mobile screens
+  const [rolling, setRolling] = useState(false);
+  const diceFaces = [
+    "Create Your",
+    "First Task!",
+    "Create Your",
+    "First Task!",
+    "Create Your",
+    "First Task!",
+  ];
+  const diceRollDelay = 1000; // Time (in milliseconds) between dice face changes
 
-  if (isMobile) {
-    return (
-      <Box
-        minH="100vh"
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-        background="linear-gradient(135deg, #3498db, #8e44ad)"
-      >
-        <Text
-          fontSize={{ base: "24px", md: "36px", lg: "48px" }}
-          fontWeight="bold"
-          color="white"
-          textAlign="center"
-          p={6}
-          borderRadius="md"
-          boxShadow="lg"
-          transformstyle="preserve-3d"
-          transition="transform 0.3s ease"
-        >
-          Create Your First Task!
-        </Text>
-      </Box>
-    );
-  }
+  const handleRollDice = () => {
+    setRolling(true);
+    setTimeout(() => {
+      setRolling(false);
+    }, diceRollDelay);
+  };
 
   return (
     <Box
@@ -43,34 +33,40 @@ const ThreeDCard = () => {
     >
       <Tilt
         options={{
-          scale: 1.1,
+          scale: 1.2, // Adjust the scale for a larger dice
           max: 25,
-          gyroscope: false, // Disable gyroscope on mobile for a consistent experience
+          gyroscope: false,
         }}
       >
         <Box
-          p={6}
-          bg="white"
-          borderRadius="md"
-          boxShadow="dark-lg"
+          p={8} // Increase padding for a larger dice
+          bgGradient="linear(to bottom, #fca5a5, #fcbacb)" // Customize the gradient colors
+          borderRadius="50%" // Make it a circle to resemble a dice
+          boxShadow="xl"
           textAlign="center"
           position="relative"
           transformstyle="preserve-3d"
           transition="transform 0.3s ease"
           borderTop={`1px solid ${borderColor}`}
           bgColor={colorMode === "light" ? "white" : "gray.700"}
-          _hover={{ transform: "rotateY(10deg)" }} // Apply hover effect only on non-mobile devices
+          onClick={handleRollDice}
+          onAnimationEnd={() => setRolling(false)}
+          _hover={!rolling && { transform: "rotateY(10deg)" }}
+          animation={rolling ? "diceRoll 2s infinite" : ""}
         >
-          <Text
-            fontSize={{ base: "24px", md: "36px", lg: "48px" }}
-            fontWeight="bold"
-            color="teal.500"
-            animation="bounce 1s infinite"
-            mb={4}
-          >
-            Create Your First Task!
-          </Text>
-
+          {diceFaces.map((face, index) => (
+            <Text
+              key={index}
+              fontSize={{ base: "20px", md: "24px", lg: "28px" }} // Adjust font sizes for different faces
+              fontWeight="bold"
+              color="white" // Change text color to white for better visibility
+              animation="bounce 1s infinite"
+              mb={2}
+              style={{ display: index === 0 ? "block" : "none" }}
+            >
+              {face}
+            </Text>
+          ))}
           {/* Shadow to create 3D effect */}
           <Box
             position="absolute"
@@ -80,7 +76,7 @@ const ThreeDCard = () => {
             bottom={0}
             bg="gray.400"
             opacity={0.2}
-            borderRadius="md"
+            borderRadius="50%" // Match the dice's border radius for a circle shadow
             pointerEvents="none"
             zIndex="-1"
             transform="translateZ(-1px)"
